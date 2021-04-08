aa58fa700acaf683760f13c88698a748b713f0da
Sunny Pacheco <pacheco.sunny@gmail.com>
2021-04-08 14:48:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -64,7 +64,8 @@ function placeOrder(cardNumber) {
   if (cardNumber === undefined) {
     return "Sorry, we don't have a credit card on file for you."
   } else if (typeof cardNumber === "number"){
-    console.log("Your total cost is $" + total() + ", which will be charged to the card" + cardNumber + ".");
+    const total = total();
     cart.splice(0);
+    return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`);
   }
 }
