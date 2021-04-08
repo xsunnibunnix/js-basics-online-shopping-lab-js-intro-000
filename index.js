678780bd595ef04093867876901d297632c3bae5
Sunny Pacheco <pacheco.sunny@gmail.com>
2021-04-08 14:17:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -61,5 +61,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber === undefined) {
+    return "Sorry, we don't have a credit card on file for you."
+  }
 }
