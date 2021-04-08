5d9b17ca99f594d3219916da24551d80f60a3fca
Sunny Pacheco <pacheco.sunny@gmail.com>
2021-04-08 14:11:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -55,10 +55,9 @@ function removeFromCart(item) {
     if (cart[i].itemName === item) {
       cart.splice(i, 1);
       console.log(cart);
-    } else {
-      return "That item is not in your cart."
     }
-  }
+}
+      return "That item is not in your cart."
 }
 
 function placeOrder(cardNumber) {
