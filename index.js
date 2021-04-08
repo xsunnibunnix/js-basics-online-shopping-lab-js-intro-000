6644f57f9b878664c8ac652be806bfaa2b257313
Sunny Pacheco <pacheco.sunny@gmail.com>
2021-04-08 14:34:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ function placeOrder(cardNumber) {
   if (cardNumber === undefined) {
     return "Sorry, we don't have a credit card on file for you."
   } else {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
     cart.splice(0);
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
   }
 }
